a44192092e54d5f0863872a3aec6d0ea7bc376cd
ganeshgovala <sriramganesh28@gmail.com>
2025-03-03 00:09:19 +0530

    Added Colors file

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xff8C00FF);
+  static const Color black = Color(0xff121212);
+  static const Color white = Color(0xffffffff);
+}
